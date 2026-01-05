ebe81b75829bc978df57a50bdf2bd6c540b58a9b
Myltiane Alves <myltiane.aux@gmail.com>
2026-01-05 11:57:02 -0300

    refactor: node modules

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import FuncionariosControllers from "./Funcionarios/controllers/index.js";
 
 const routes = new Router();
 // routes.use(authMiddleware)
@@ -8,6 +8,12 @@ routes.get('/', (req, res) => {
     res.send('Hello World! Myltiane');
 });
 
+//  Funcionarios 
+
+routes.get('/autorizarVoucher', FuncionariosControllers.getAutorizacaoVoucherFuncionarios)
+// routes.get('/listaFuncionariosEmpresa', FuncionariosControllers.getListaFuncionariosEmpresa)
+routes.get('/funcionario-ativo-por-empresa', FuncionariosControllers.getListaFuncionariosAtivos)
+routes.get('/todos-funcionario', FuncionariosControllers.getListaTodosFuncionarios)
 
 export default routes;
 
