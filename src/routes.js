e00e6762685a8b276c91e71f1942a397602ba561
Myltiane Alves <myltiane.aux@gmail.com>
2026-01-05 12:00:31 -0300

    refactor: rotas

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+
 import FuncionariosControllers from "./Funcionarios/controllers/index.js";
+import EmpresaControllers from "./Empresas/controllers/Empresas.js"
 
 const routes = new Router();
 // routes.use(authMiddleware)
@@ -14,6 +16,12 @@ routes.get('/autorizarVoucher', FuncionariosControllers.getAutorizacaoVoucherFun
 // routes.get('/listaFuncionariosEmpresa', FuncionariosControllers.getListaFuncionariosEmpresa)
 routes.get('/funcionario-ativo-por-empresa', FuncionariosControllers.getListaFuncionariosAtivos)
 routes.get('/todos-funcionario', FuncionariosControllers.getListaTodosFuncionarios)
+
+routes.get('/empresas', EmpresaControllers.getAllEmpresas);
+routes.get('/grupoEmpresarial', EmpresaControllers.getAllGrupoEmpresarial);
+routes.get('/subGrupoEmpresarial', EmpresaControllers.getSelectLojaVouchers);
+routes.get('/listaEmpresas', EmpresaControllers.getListaEmpresas)
+routes.put('/empresas/:id', EmpresaControllers.putListaEmpresas)
 
 export default routes;
 
