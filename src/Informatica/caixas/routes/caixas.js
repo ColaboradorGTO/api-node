d29741f8ebfaf4ce35f78cc383f9565436487b76
Myltiane Alves <myltiane.aux@gmail.com>
2026-01-05 11:53:59 -0300

    refactor: node modules

--- src/Informatica/caixas/routes/caixas.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express' ;
-//import CaixaControllers from '.././controllers/controllersCaixas'
-const routes = new Router();
-
-routes.post('/criar-caixas', CaixaControllers.postCaixaLojas)
-routes.put('/lista-caixas/:id', CaixaControllers.putCaixaLoja)
-routes.put('/atualiza-empresa-diario/:id', CaixaControllers.putAtualizaEmpresaDiario)
-routes.put('/atualizar-todos-caixa', CaixaControllers.putAtualizarTodosCaixas)
-export default routes
