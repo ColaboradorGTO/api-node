050d4f17c1ef3f84895c9829087b3000946fee21
Myltiane Alves <myltiane.aux@gmail.com>
2026-01-07 09:15:57 -0300

    reafactor: api DESCONTO MOTIVO VENDAS

--- src/Administrativo/Desconto/controllers/desconto.js
+++ src/Administrativo/Desconto/controllers/desconto.js
@@ -18,8 +18,8 @@ class AdmDescontoControllers {
 
         try {
 
-            const apiUrl = `http://164.152.245.77:8000/quality/concentrador/api/administrativo/desconto-motivo-vendas.xsjs?idMarca=${idGrupo}&idEmpresa=${idEmpresa}&dataInicial=${dataPesquisaInicio}&dataFinal=${dataPesquisaFim}&dsmotdesc=${dsMotivoDesc}`
-            
+            const apiUrl = `http://164.152.245.77:8000/quality/concentrador/api/administrativo/desconto-motivo-vendas.xsjs?idMarca=${idGrupo}&idEmpresa=${idEmpresa}&dataInicial=${dataPesquisaInicio}&dataFinal=${dataPesquisaFim}&dsmotdesc=${dsMotivoDesc}&page=${page}&pageSize=${pageSize}`;
+           
             const response = await axios.get(apiUrl)
 
             return res.json(response.data);
