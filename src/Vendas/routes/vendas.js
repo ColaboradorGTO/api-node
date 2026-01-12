657fd6b85fa1b7ecaf62f440e94d1d02d0315f83
Myltiane Alves <myltiane.aux@gmail.com>
2026-01-12 09:34:23 -0300

    reafactor: package.json

--- src/Vendas/routes/vendas.js
+++ src/Vendas/routes/vendas.js
@@ -4,6 +4,10 @@ import VendasControllers from '../controllers/Vendas.js';
 
 const routes = new Router();
 
+
+// routes.get('/listaVendas', FinanceiroVendasControllers.getListaVendas)
+
+// routes.get('/listaVendaCliente', Vendas.getListaVendaCliente)
 routes.get('/movimentacaoSaldo', VendasControllers.getListaVendasSaldo)
 routes.get('/rotatividadeVendas', VendasControllers.getListaRotatividade)
 routes.get('/listaDetalheVendaCliente', VendasControllers.getListaDetalheVendaCliente)
