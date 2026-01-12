bfb6882c6a4f7cb72af2cc947756b0e58d1b3f78
Myltiane Alves <myltiane.aux@gmail.com>
2026-01-12 09:38:19 -0300

    reafactor: package.json

--- src/routes.js
+++ src/routes.js
@@ -31,9 +31,9 @@ import PromocaoControllers from './Promocao/controllers/Promocao.js'
 import CaixaControllers from './Informatica/caixas/controllers/controllersCaixas.js'
 import RelatorioBIController from './Informatica/relatorio/ralatoriaBI/controller/controllerRelatorioBi.js';
 import LinkRelatorioBiController from './Informatica/relatorio/linkRelatorioBI/controllers/controllersLinkRelatorioBi.js'
-import ConsultaNFceController  from './Informatica/ConsultaNFCE/controllers/index.js'
-import ConsultaNFeController from './Informatica/ConsultaNFCE/controllers/nfe.js'
-import ConsultaStatusNfeController from './Informatica/ConsultaNFCE/controllers/statusNfce.js'
+// import ConsultaNFceController  from './Informatica/ConsultaNFCE/controllers/index.js'
+// import ConsultaNFeController from './Informatica/ConsultaNFCE/controllers/nfe.js'
+// import ConsultaStatusNfeController from './Informatica/ConsultaNFCE/controllers/statusNfce.js'
 
 // Financeiro Início
 import AdiantamentosControllers from './Financeiro/Adiantamentos/controllers/adiantamentos.js'
@@ -475,13 +475,13 @@ routes.put('/funcionarios-desconto/:id', InformaticaControllers.putFuncionarioDe
 // routes.post('/consulta-nfec', ConsultaNfeController.consultar)
 // routes.get('/valida-venda-contingencia', ConsultaNfeController.getListaVendasContigenciaValidas);
 // routes.put('/valida-venda-contingencia/:id', ConsultaNfeController.putValidarVendaContigencia);
-routes.get('/status-sefaz', ConsultaStatusNfeController.statusSefaz);
-routes.post('/consultar-nfce', ConsultaNFceController.consultaNFce);
-routes.post('/downloadXML', ConsultaStatusNfeController.downloadNFE);
-routes.post('/cancelar-nfe', ConsultaStatusNfeController.cancelarNFE);
-routes.post('/inutilizar-nfe', ConsultaStatusNfeController.inutilizarNFE);
-routes.get('/validarConsulta', ConsultaStatusNfeController.validarConsulta);
-routes.post('/consultar-nfe', ConsultaNFeController.consultaNFe);
+// routes.get('/status-sefaz', ConsultaStatusNfeController.statusSefaz);
+// routes.post('/consultar-nfce', ConsultaNFceController.consultaNFce);
+// routes.post('/downloadXML', ConsultaStatusNfeController.downloadNFE);
+// routes.post('/cancelar-nfe', ConsultaStatusNfeController.cancelarNFE);
+// routes.post('/inutilizar-nfe', ConsultaStatusNfeController.inutilizarNFE);
+// routes.get('/validarConsulta', ConsultaStatusNfeController.validarConsulta);
+// routes.post('/consultar-nfe', ConsultaNFeController.consultaNFe);
 
 // routes.put('/configuracao-todos/:id', InformaticaControllers.putCaixaLoja)
 // FIM Informática
