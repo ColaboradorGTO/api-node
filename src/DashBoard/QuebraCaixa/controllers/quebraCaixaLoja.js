49d0f58f7769f0f3579183791126f7ed95a53d37
Myltiane Alves <myltiane.aux@gmail.com>
2026-01-23 13:48:18 -0300

    reafactor:  venda-recebido eletronico

--- src/DashBoard/QuebraCaixa/controllers/quebraCaixaLoja.js
+++ src/DashBoard/QuebraCaixa/controllers/quebraCaixaLoja.js
@@ -42,6 +42,7 @@ class QuebraCaixaControllers {
         try {
 
             const apiUrl = `${url}/api/dashboard/quebra-caixa/lista-quebra-caixa.xsjs?pageSize=${pageSize}&page=${page}&idEmpresa=${idEmpresa}&dataPesquisaInic=${dataPesquisaInicio}&dataPesquisaFim=${dataPesquisaFim}&idMarca=${idMarca}&cpfquebraop=${cpfOperadorQuebra}&stQuebraPositivaNegativa=${stQuebraPositivaNegativa}&uf=${uf}`;
+            // const apiUrl = `http://164.152.245.77:8000/quality/concentrador_homologacao/api/dashboard/quebra-caixa/lista-quebra-caixa.xsjs?pageSize=${pageSize}&page=${page}&idEmpresa=${idEmpresa}&dataPesquisaInic=${dataPesquisaInicio}&dataPesquisaFim=${dataPesquisaFim}&idMarca=${idMarca}&cpfquebraop=${cpfOperadorQuebra}&stQuebraPositivaNegativa=${stQuebraPositivaNegativa}&uf=${uf}`;
             const response = await axios.get(apiUrl);
 
             return res.json(response.data);
