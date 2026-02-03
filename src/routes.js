094644ad3af58cca85a0fdd5769c93873aab4652
Myltiane Alves <myltiane.aux@gmail.com>
2026-02-03 10:49:59 -0300

    feat: add postIntegrarPagamentoPixSAP

--- src/routes.js
+++ src/routes.js
@@ -794,6 +794,9 @@ routes.put('/desativar-status-promocao', PromocaoControllers.putStatusPromocao)
 routes.post('/deposito-integracao', ServiceLayerControllers.postDepositoIntegrarNoSAP)
 routes.post('/adiantamentos-salariais-integracao', ServiceLayerControllers.postIntegrarAdiantamentoSalarial)
 routes.post('/quebras-de-caixas-integracao', ServiceLayerControllers.postIntegrarQuebraCaixaSAP)
+routes.post('/pix-integracao', ServiceLayerControllers.postIntegrarPagamentoPixSAP)
+
+
 // // Logs
 // routes.get('/log-web', LogsControllers.getListaLogsUsuario)
 routes.post('/log-web', LogsControllers.createLogsUsuario)
